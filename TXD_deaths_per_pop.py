aefc5b68dc95dd28a288a9719cc78c383f62c735
annieyintingxu <67021179+annieyintingxu@users.noreply.github.com>
2020-06-30 14:29:55 -0700

    Update and rename JHU_deaths_per_pop.py to TXD_deaths_per_pop.py

--- TXD_deaths_per_pop.py
+++ TXD_deaths_per_pop.py
@@ -5,12 +5,12 @@ import math
 #county population size in col 2
 #daily new death count starting col 3               
 
-JHU_df = pd.read_csv('/Users/Dino-Sunlight/Desktop/COVIDeeDesktop/Texas COVID-19 Fatality Count Data by County 6_15_20.csv') #loads data
+TXD_df = pd.read_csv('/Users/Dino-Sunlight/Desktop/COVIDeeDesktop/Texas COVID-19 Fatality Count Data by County 6_15_20.csv') #loads data
 #print("The shape of the DataFrame JHU_df is", JHU_df.shape) #okay it is recognizing dimensions correctly
 
 county_data = {}
 row_count = 0 #initialize row_count
-for row in JHU_df.itertuples(): #this line: names each row as an index...??? below: calculates and stores the total and per capita for each row in the dataframe. Can I just use "JHU_df"??
+for row in TXD_df.itertuples(): #this line: names each row as an index...??? below: calculates and stores the total and per capita for each row in the dataframe. Can I just use "JHU_df"??
     county_total_deaths = 0 #resets values for each county
     county_total_deaths_per_capita = 0
     if row[0] > 1 and row_count > 2 and row_count < math.inf: #start on first county line. Also yes, the csv inputs are recognizes as strings wth
@@ -29,7 +29,7 @@ for row in JHU_df.itertuples(): #this line: names each row as an index...??? bel
 
 calculated_df = pd.DataFrame.from_dict(county_data, orient = 'index') #treats county names column as index rather than as list of its own
 calculated_df.columns = ['Cumulative Deaths','Cumulative Deaths per Capita']
-calculated_df.to_csv('JHU_Calculated.csv') #I want to name by date somehow (ugh gotta extract from original csv)
+calculated_df.to_csv('TXD_Calculated.csv') #I want to name by date somehow (ugh gotta extract from original csv)
 
 #test    
 print(calculated_df.head())
